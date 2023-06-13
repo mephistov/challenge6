ba34a7746138798b427e9ee06ba65561d38f4a00
GLOBANT\nicolas.castilla <nicolas.castilla@globant.com>
2023-06-13 14:00:37 -0500

    finish profile

--- app/src/main/java/com/nicolascastilla/challenge6/composables/ProfileComposable.kt
+++ app/src/main/java/com/nicolascastilla/challenge6/composables/ProfileComposable.kt
@@ -1,11 +1,13 @@
 package com.nicolascastilla.challenge6.composables
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
@@ -15,14 +17,19 @@ import com.nicolascastilla.challenge6.viewmodels.MainViewModel
 
 @Composable
 fun ProfileComposable(navController: NavHostController, viewModel: MainViewModel) {
-    Column(modifier = Modifier.padding(16.dp)) {
+    viewModel.gerUserInfo()
+    Column(
+        modifier = Modifier.padding(16.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
         Image(
             painter = painterResource(id = R.drawable.avatar),
             contentDescription = null,
             modifier = Modifier.size(100.dp)
         )
-        Text(text = "userProfile.name", )
-        Text(text = "userProfile.phoneNumber", )
-        Text(text = "userProfile.email", )
+        Text(text = viewModel.nameProfile.value, )
+        Text(text = viewModel.phoneProfile.value, )
+        Text(text = viewModel.emailProfile.value, )
     }
 }
--- app/src/main/java/com/nicolascastilla/challenge6/viewmodels/MainViewModel.kt
+++ app/src/main/java/com/nicolascastilla/challenge6/viewmodels/MainViewModel.kt
@@ -4,6 +4,7 @@ import android.util.Log
 import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.ktx.database
 import com.google.firebase.ktx.Firebase
@@ -11,14 +12,18 @@ import com.nicolascastilla.challenge6.ui.theme.BlueGradient
 import com.nicolascastilla.challenge6.ui.theme.StrokeColor
 import com.nicolascastilla.domain.repositories.entities.MessageItem
 import com.nicolascastilla.domain.repositories.entities.messages.UserChatEntity
+import com.nicolascastilla.domain.repositories.usecases.interfaces.GetAutenticationUseCase
 import com.nicolascastilla.domain.repositories.usecases.interfaces.GetInfoUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class MainViewModel @Inject constructor(
-    private val getInfoUseCase: GetInfoUseCase
+    private val getInfoUseCase: GetInfoUseCase,
+    private val userUseCase: GetAutenticationUseCase
 ):ViewModel() {
 
     val goToChat = MutableLiveData<HashMap<String,String>>()
@@ -31,6 +36,23 @@ class MainViewModel @Inject constructor(
         tempHasmap.put("name",item.name)
         tempHasmap.put("idChat",item.idChat)
         goToChat.value= tempHasmap
+    }
+
+    val nameProfile =  mutableStateOf<String>("")
+    val emailProfile =  mutableStateOf("")
+    val phoneProfile =  mutableStateOf("")
+    fun gerUserInfo(){
+        viewModelScope.launch {
+            val data = userUseCase.isUserLogged()
+            val user = data.first()
+            user?.let {
+                nameProfile.value = it.name
+                emailProfile.value = it.emai
+                phoneProfile.value = it.phone
+            }
+
+
+        }
     }
 
     val myItems: Flow<List<UserChatEntity>> = getInfoUseCase.getAllUserInfo()
